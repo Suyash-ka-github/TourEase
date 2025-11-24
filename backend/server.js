df8b3cfa33c81810937e80b572f3f89b7b2a3dd8
Suhani Garg <suhanigarg59@gmail.com>
2025-11-24 16:56:46 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -35,10 +35,7 @@ app.use(cors({
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
 }));
 
-app.options((req, res) => {
-  res.sendStatus(200);
-});
-
+app.options("/*", cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
